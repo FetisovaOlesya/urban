92e5261ea2662912608409c24001463f763c7b3e
fetisova1 <fetisova@individ.ru>
2024-11-23 22:35:53 +0300

    задания по вводному и первому модулю

---
+++ src/main/java/org/example/JavaIfSwitchReturn.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class JavaIfSwitchReturn {
+    public static void main(String[] args) {
+        part1();
+        part2();
+    }
+
+    public static void part1() {
+        int a = 8;
+        int b = 4;
+        if ((a % 2 == 0 && b % 2 == 0) || ((a % 2 !=0 && b % 2 !=0))) {
+                System.out.println("Удачное совпадение");
+        }
+    }
+
+    public static void part2() {
+        int day = 3;
+        if (day >=1 && day <= 10){
+            System.out.println("Первая декада");
+        } else if (day >=11 && day <=20) {
+            System.out.println("Вторая декада");
+        }else if (day >=21 && day <=31) {
+            System.out.println("Третья декада");
+        }
+    }
+}
