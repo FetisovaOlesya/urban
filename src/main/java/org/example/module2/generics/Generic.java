d5f28c089d454746c9b463b0e66505c616e6d6ff
fetisova1 <fetisova@individ.ru>
2024-12-08 01:20:18 +0300

    Домашнее задание по теме "Generics autoboxing autounboxing"

---
+++ src/main/java/org/example/module2/generics/Generic.java
@@ -0,0 +1,38 @@
+package org.example.module2.generics;
+
+public class Generic<T, V> {
+
+    public static <T, V> boolean isEqualsArray(T[] arrOne, T[] arrTwo) {
+        if (arrOne.getClass() != arrTwo.getClass()) {
+            System.out.println("Массивы разных типов!");
+            return false;
+        }
+        if (arrOne.length != arrTwo.length) {
+            System.out.println("Массивы не равны по длине!");
+            return false;
+        }
+
+        for (int i=0; i < arrOne.length; i++) {
+            if (!arrOne[i].equals(arrTwo[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static <T> T[] changePositionInArray(T[] array, int positionOne, int positionTwo) throws Exception {
+        if (positionOne <0 || positionOne >= array.length) {
+            throw new Exception("positionOne за рамками возможного диапазона");
+        }
+        if (positionTwo <0 || positionTwo >= array.length) {
+            throw new Exception("positionOne за рамками возможного диапазона");
+        }
+
+        T tmp = array[positionOne];
+        array[positionOne] = array[positionTwo];
+        array[positionTwo] = tmp;
+        return array;
+    }
+
+
+}
